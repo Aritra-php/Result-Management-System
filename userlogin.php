fc3294168e1fcce3bbf7166a57f4f9ebda9be245
Subhankar Pandey <subhankarpandey91@gmail.com>
2021-03-16 12:32:45 +0530

    change title

--- userlogin.php
+++ userlogin.php
@@ -63,7 +63,7 @@ echo '<script>location.href="userprofile.php"</script>';
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-<title>UserLogin.com</title>
+<title>www.UserLogin.com</title>
 </head>
 <body>
 <div class="container">
